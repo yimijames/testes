2ad17fac0b40d652571be471b1475d67a52a8238
jamestzungcheng <jamestzungcheng@gmail.com>
2024-08-21 10:54:56 -0300

    first commit

---
+++ Consumidor/Consumidor.cs
@@ -0,0 +1,47 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System;
+
+class Program
+{
+    static IConnectionFactory connectionFactory = null!;
+    static IConnection connection = null!;
+    static ISession session = null!;
+    static IDestination destination = null!;
+    static IMessageConsumer consumer = null!;
+
+    static void Main(string[] args)
+    {
+        // Conectar ao ActiveMQ
+        ConnectActiveMQ();
+
+        Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // Iniciar consumo de mensagens
+        consumer.Listener += new MessageListener(OnMessageReceived);
+
+        // Manter o aplicativo rodando para receber mensagens
+        Console.ReadLine();
+
+        connection.Close();
+    }
+
+    static void ConnectActiveMQ()
+    {
+        connectionFactory = new ConnectionFactory("tcp://localhost:61616");
+        connection = connectionFactory.CreateConnection();
+        connection.Start();
+        session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+        destination = session.GetQueue("temperature_queue");
+        consumer = session.CreateConsumer(destination);
+    }
+
+    static void OnMessageReceived(IMessage message)
+    {
+        if (message is ITextMessage textMessage)
+        {
+            string body = textMessage.Text;
+            Console.WriteLine($" [x] Received {body}");
+        }
+    }
+}
---
+++ Produtor/Produtor.cs
@@ -0,0 +1,57 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System;
+using System.Threading;
+
+class Program
+{
+    static IConnectionFactory connectionFactory;
+    static IConnection connection;
+    static ISession session;
+    static IDestination destination;
+    static IMessageProducer producer;
+
+    static void Main(string[] args)
+    {
+        // Conectar ao ActiveMQ
+        ConnectActiveMQ();
+
+        try
+        {
+            Random random = new Random();
+            while (true)
+            {
+                double temperature = random.NextDouble() * (40 - 20) + 20;
+                Console.WriteLine($"Current temperature: {temperature:F2} degrees");
+                if (temperature >= 35)
+                {
+                    PublishTemperature(temperature);
+                }
+                Thread.Sleep(5000); // Pausa de 5 segundos
+            }
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    static void ConnectActiveMQ()
+    {
+        connectionFactory = new ConnectionFactory("tcp://localhost:61616");
+        connection = connectionFactory.CreateConnection();
+        connection.Start();
+        session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+        destination = session.GetQueue("temperature_queue");
+        producer = session.CreateProducer(destination);
+        producer.DeliveryMode = MsgDeliveryMode.Persistent;
+    }
+
+    static void PublishTemperature(double temperature)
+    {
+        string message = $"Temperature reached {temperature:F2} degrees";
+        ITextMessage textMessage = producer.CreateTextMessage(message);
+        producer.Send(textMessage);
+        Console.WriteLine($" [x] Sent '{message}'");
+    }
+}
